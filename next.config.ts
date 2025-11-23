10c85c92d7a31a208f2c8a19a8e4acdc41b3c93c
Zerui Chen <zerui2112@gmail.com>
2025-11-23 17:52:53 +0000

    Add assetPrefix and trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
   // If deploying to https://<USERNAME>.github.io/<REPO_NAME>/
   // Set basePath to '/<REPO_NAME>'
   basePath: '/qconfig',
+  assetPrefix: '/qconfig/', // Asset prefix is often needed for static exports in subpaths
+  trailingSlash: true, // Recommended for GitHub Pages
 };
 
 export default nextConfig;
